cda14843fc42bba9bf2c69c8a9d35079d35e09cf
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-02-28 14:54:47 +0530

    Add files via upload

---
+++ convert_kilometers_to_miles.py
@@ -0,0 +1,13 @@
+#collect input from the user
+
+kilometers = float(input('How many kilometers: '))
+
+#conversion factor
+
+conv_fac = 0.621371
+
+#convert miles
+
+miles = kilometers * conv_fac
+
+print("%0.3f kilometers is equal to %0.3f miles" %(kilometers,miles))
---
+++ display_calender.py
@@ -0,0 +1,10 @@
+import calendar
+
+#enter the month and year
+
+yy = int(input("Enter the year: "))
+mm = int(input("Enter the month: "))
+
+#display the calendar
+
+print(calendar.month(yy,mm))
