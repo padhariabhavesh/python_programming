76b8ffe08037085a48cf171d3c77e59ac5b298d1
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-06-08 10:28:25 +0530

    Update url-shortner.py

--- url-shortner.py
+++ url-shortner.py
@@ -1,5 +1,5 @@
 import pyshorteners
-url = ("https://www.youtube.com/watch?v=dXHYE9Zf7YY&t=47s")
+url = ("https://www.youtube.com/channel/UC1SB4wu5760RERv_ujVPz2g?view_as=subscriber")
 s = pyshorteners.Shortener()
 print("your shortened url is:  ",s.tinyurl.short(url))
 
